689796f1c3f5d0a4a18ebb9a57bd73a2fabe1c90
CameronM <cameron.mullins@hotmail.fr>
2018-12-04 17:03:38 +1100

    Portal Fixes

---
+++ CIU Game Project/Assets/Scripts/PortalTeleporter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleporter : MonoBehaviour {
+
+    public Transform player;
+    public Transform reciever;
+
+    private bool playerIsOverLapping = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( playerIsOverLapping)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            if (dotProduct < 0f)
+            {
+                // Teleport him!
+                float rotationDiff = Quaternion.Angle(transform.rotation, reciever.rotation);
+                rotationDiff += 180;
+                player.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f)* portalToPlayer;
+                player.position = reciever.position + positionOffset;
+
+                playerIsOverLapping = false;
+            }
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerIsOverLapping = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverLapping = false;
+        }
+    }
+}
